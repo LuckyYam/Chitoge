9ed776c4ebf407a6afefe4c952aaee4177045f91
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-13 16:36:06 +0530

    Update Slap.ts

--- src/commands/Fun/Slap.ts
+++ src/commands/Fun/Slap.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'slap',
             description: 'Slap someone',
-            category: 'reactions2',
+            category: 'fun',
             usage: `${client.config.prefix}slap [tag/quote users]`,
         })
     }
@@ -46,8 +46,8 @@ export default class Command extends BaseCommand {
             MessageType.video,
             Mimetype.gif,
             [M.sender.jid, ...M.mentioned],
-            `*@${M.sender.jid.split('@')[0]} Slapped ${M.mentioned
-                .map((user) => (user === M.sender.jid ? 'Themselves' : `@${user.split('@')[0]}`))
+            `*@${M.sender.jid.split('@')[0]} slapped ${M.mentioned
+                .map((user) => (user === M.sender.jid ? 'themselves' : `@${user.split('@')[0]}`))
                 .join(', ')}*`
         )
     }
