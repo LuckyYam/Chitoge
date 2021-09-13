27e98f001286ddb8a9ec836eb32b4ac17ae4f334
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-13 17:42:29 +0530

    Update Kaoi.ts

--- src/commands/Misc/Kaoi.ts
+++ src/commands/Misc/Kaoi.ts
@@ -1,23 +1,41 @@
 import { MessageType } from '@adiwajshing/baileys'
-import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'kaoi',
+            command: 'chitoge',
             description: 'Displays the info',
             category: 'misc',
-            usage: `${client.config.prefix}kaoi`,
+            usage: `${client.config.prefix}chitoge`,
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void M.reply(`👾 *kaoi* 👾\n\n🍀 *Description:* Maintained Fork of WhatsApp Botto Void\n\n🌐 *URL:* https://github.com/PrajjwalDatir/Kaoi#readme\n\n📂 *Repository:* https://github.com/PrajjwalDatir/Kaoi`
-        ).catch((reason: any) =>
-            M.reply(`an error occupered, Reason: ${reason}`))
+        
+
+        
+        
+return void M.reply(await request.buffer('https://i.pinimg.com/736x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg'),
+MessageType.image,
+            undefined,
+            undefined,
+            `🌟 *Chitoge* 🌟 \n\n🍀 *Description:* Maintained Fork of WhatsApp Botto Void\n\n🌐 *URL:* https://github.com/ShinNouzen/Chitoge#readme\n\n📂 *Repository:* https://github.com/ShinNouzen/Chitoge`
+           
+
+)
+
+
     }
+
+
+
+
+
+
+
 }
