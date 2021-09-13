79cee3a0684554bbaf610642c0d6c3963d64e758
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-13 17:01:27 +0530

    Add files via upload

---
+++ src/commands/Fun/slash.ts
@@ -0,0 +1,36 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'slash',
+            description: 'Slash someone!!',
+            category: 'fun',
+            usage: `${client.config.prefix}slash @whom_you_wanna_slash`
+        })
+    }
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const user1 = M.sender.jid
+        const user2 = M.mentioned[0]
+        //  let username1 = user1.split('@')[0]
+        //  let username2 = user2.split('@')[0]
+        // let username1 = user1.replace('@s.whatsapp.net', '')
+        // let username2 = user2.replace('@s.whatsapp.net', '')
+        const n = [
+            './assets/videos/slash/slash2.mp4',
+            './assets/videos/slash/slash1.mp4',
+            './assets/videos/slash/slash.mp4'
+        ]
+        let hug = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: hug }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `@${user1.split('@')[0]} slashed @${user2.split('@')[0]}`,
+            contextInfo: { mentionedJid: [user1, user2] }
+        })
+    }
+}
