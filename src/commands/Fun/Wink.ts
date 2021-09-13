4af805aa381fdd6b60c020f005e713aab21f2152
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-13 16:40:19 +0530

    Update Wink.ts

--- src/commands/Fun/Wink.ts
+++ src/commands/Fun/Wink.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'wink',
             description: 'Wink at someone',
-            category: 'reactions2',
+            category: 'fun',
             usage: `${client.config.prefix}wink [tag/quote users]`,
         })
     }
@@ -46,8 +46,8 @@ export default class Command extends BaseCommand {
             MessageType.video,
             Mimetype.gif,
             [M.sender.jid, ...M.mentioned],
-            `*@${M.sender.jid.split('@')[0]} Winked at ${M.mentioned
-                .map((user) => (user === M.sender.jid ? 'Themselves' : `@${user.split('@')[0]}`))
+            `*@${M.sender.jid.split('@')[0]} winked at ${M.mentioned
+                .map((user) => (user === M.sender.jid ? 'themselves' : `@${user.split('@')[0]}`))
                 .join(', ')}*`
         )
     }
