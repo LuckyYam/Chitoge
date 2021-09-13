934a4499f9e329ffcd03522c49d1e5ef1b7f7ea7
ShinNouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-13 17:19:30 +0530

    Update Xp.ts

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'xp',
-            description: "Displays User's Xp 🌟",
+            description: "Displays User's Xp ⭐",
             category: 'general',
             usage: `${client.config.prefix}xp (@tag)`,
             aliases: ['exp']
