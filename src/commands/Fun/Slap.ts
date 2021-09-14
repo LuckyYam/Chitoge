8d6b2cf37ed1ec70290aa47599b2e2c7b107e813
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-14 07:54:42 +0530

    Update Slap.ts

--- src/commands/Fun/Slap.ts
+++ src/commands/Fun/Slap.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'slap',
             description: 'Slap someone',
-            category: 'fun',
+            category: 'fun2',
             usage: `${client.config.prefix}slap [tag/quote users]`,
         })
     }
