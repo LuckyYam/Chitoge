11c719e69bb317241a6f57fe48f2b483ff8f04e2
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-14 07:53:14 +0530

    Update Famousquote.ts

--- src/commands/Fun/Famousquote.ts
+++ src/commands/Fun/Famousquote.ts
@@ -10,7 +10,7 @@ export default class Command extends BaseCommand {
             command: 'fquote',
             description: 'Gives you random famous quote.',
             aliases: ['fq'],
-            category: 'fun',
+            category: 'fun2',
             usage: `${client.config.prefix}fquote`
         })
     }
