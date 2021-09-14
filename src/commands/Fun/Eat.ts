a1d15b5931947e34070e58e127bf9227a5b7dd87
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-14 07:54:20 +0530

    Update Eat.ts

--- src/commands/Fun/Eat.ts
+++ src/commands/Fun/Eat.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'eat',
             description: 'You like to eat someone',
-            category: 'fun',
+            category: 'fun2',
             usage: `${client.config.prefix}eat [tag/quote users]`,
         })
     }
