8455a529f1222d2d0cf8c3663ce8c18a8099ca97
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-14 07:53:52 +0530

    Update Ultrahug.ts

--- src/commands/Fun/Ultrahug.ts
+++ src/commands/Fun/Ultrahug.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'ultrahug',
             description: 'Gives Ultra Hug to someone special',
-            category: 'fun',
+            category: 'fun2',
             usage: `${client.config.prefix}ultrahug [tag/quote users]`,
         })
     }
