95e26bce72bdb62655246c12ae9b5eef48a0ffc8
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-14 07:55:14 +0530

    Update Cuddle.ts

--- src/commands/Fun/Cuddle.ts
+++ src/commands/Fun/Cuddle.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'cuddle',
             description: 'Cuddle someone',
-            category: 'fun',
+            category: 'fun2',
             usage: `${client.config.prefix}cuddle [tag/quote users]`,
         })
     }
