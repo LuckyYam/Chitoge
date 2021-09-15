d1214b01ce61326fa7f5708a8470323011d71a8a
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-15 14:58:33 +0530

    Update Smug.ts

--- src/commands/Fun/Smug.ts
+++ src/commands/Fun/Smug.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'smug',
             description: 'Smug at someone',
-            category: 'fun2',
+            category: 'fun-2',
             usage: `${client.config.prefix}smug [tag/quote users]`,
         })
     }
