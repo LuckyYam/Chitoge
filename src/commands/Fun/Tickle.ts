e5d832f20cf02dd86cf7c82712ce6936277f5b6f
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-15 14:58:48 +0530

    Update Tickle.ts

--- src/commands/Fun/Tickle.ts
+++ src/commands/Fun/Tickle.ts
@@ -10,7 +10,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'tickle',
             description: 'Tickle someone',
-            category: 'fun2',
+            category: 'fun-2',
             usage: `${client.config.prefix}tickle [tag/quote users]`
         })
     }
