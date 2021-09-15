86b4cb7c8911be19b124194fd7da174b67b58361
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-15 14:56:47 +0530

    Update Hold.ts

--- src/commands/Fun/Hold.ts
+++ src/commands/Fun/Hold.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'hold',
             description: 'Hold hands with someone',
-            category: 'fun2',
+            category: 'fun-2',
             usage: `${client.config.prefix}hold [tag/quote users]`,
         })
     }
