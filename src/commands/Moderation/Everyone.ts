aec41b7b8893abe9065c6822c3962d72fb1dd1a6
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-16 21:09:45 +0530

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -21,6 +21,6 @@ export default class Command extends BaseCommand {
             undefined,
             undefined,
             M.groupMetadata?.participants.map((user) => user.jid)
-        ).catch((reason: any) => M.reply(`an error occupered, Reason: ${reason}`)))
+        ).catch((reason: any) => M.reply(`✖ An error occurred, Reason: ${reason}`)))
     }
 }
