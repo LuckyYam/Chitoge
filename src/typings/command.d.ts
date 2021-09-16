6bac2919bd07734534685bb5cdea8bc6adcf571a
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-16 13:58:30 +0530

    Update command.d.ts

--- src/typings/command.d.ts
+++ src/typings/command.d.ts
@@ -19,4 +19,4 @@ export interface ICommand {
     }
 }
 
-export type TCategory = 'general' | 'moderation' | 'misc' | 'media' | 'utils' | 'dev' | 'category' | 'fun' | 'weeb' | 'reactions'
+export type TCategory = 'general' | 'moderation' | 'misc' | 'media' | 'utils' | 'dev' | 'category' | 'fun' | 'fun-2' | 'weeb' | 'nsfw'
