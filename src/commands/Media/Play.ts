684d05c590137318da98fcc0599379d4ac816e99
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-17 18:06:15 +0530

    Update and rename play.ts to Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: videos[0].title.substr(0, 30),
-                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Kaoi`,
+                        body: `author : ${videos[0].author.name.substr(0, 20)}\n🌟 Chitoge 🌟`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
