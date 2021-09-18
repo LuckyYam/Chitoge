1bc0bf7344a3a2b255b772799855366c4888ceda
well300 <90952986+well300@users.noreply.github.com>
2021-09-19 00:28:01 +0530

    Create do_you_mean.ts

---
+++ src/commands/General/do_you_mean.ts
@@ -0,0 +1,42 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '',
+         
+
+        })
+    }
+
+
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+
+
+
+return void M.reply(await request.buffer('https://wallpapercave.com/uwp/uwp1414983.png'),
+MessageType.image,
+            undefined,
+            undefined,
+            `*No such command! Aah...*`
+
+
+)
+
+
+    }
+
+
+
+
+
+
+
+}
