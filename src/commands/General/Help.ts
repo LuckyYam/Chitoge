84446f038c5cd9a65b0c7f59d31f2483c6f5c396
well300 <90952986+well300@users.noreply.github.com>
2021-09-19 16:49:48 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,10 +1,10 @@
-import MessageHandler from '../../Handlers/MessageHandler'
+'../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
-import { MessageType } from '@adiwajshing/baileys'
 import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
 
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
@@ -13,7 +13,8 @@ export default class Command extends BaseCommand {
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            aliases: ['h']
+            dm: true,
+            aliases: ['menu']
         })
     }
 
@@ -31,16 +32,20 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `🌟 *Chitoge's Command List* 🌟\n\n`
+            let text = `✇ Kakashi's Command List ✇\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
+                text += `${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}\n❐ \`\`\`${categories[
                     key
                 ]
                     .map((command) => command.config?.command)
                     .join(', ')}\`\`\`\n\n`
-            return void M.reply( 
-                `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
+            return void M.reply( await request.buffer('https://wallpapercave.com/uwp/uwp1415142.png'),  
+                                MessageType.image,            
+                                undefined,
+                                undefined,
+
+                `${text} 🗃️ Note: Use ${this.client.config.prefix}help <command_name> to view the command info`
             )
         }
         const key = parsedArgs.joined.toLowerCase()
@@ -48,17 +53,17 @@ export default class Command extends BaseCommand {
         if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
         M.reply(
-            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
+            `🎫 Command: ${this.client.util.capitalize(command.config?.command)}\n🎗️ Status: ${
                 state ? 'Disabled' : 'Available'
-            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+            }\n🀄 Category: ${this.client.util.capitalize(command.config?.category || '')}${
                 command.config.aliases
-                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    ? `\n🍥 Aliases: ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
                     : ''
-            }\n🎐 *Group Only:* ${this.client.util.capitalize(
+            }\n🃏 Group Only: ${this.client.util.capitalize(
                 JSON.stringify(!command.config.dm ?? true)
-            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+            )}\n🎀 Usage: ${command.config?.usage || ''}\n\n🔖 Description: ${command.config?.description || ''}`
         )
     }
 
-    emojis = ['♟', '♻️', '🌈', '🎵', '❄', '👑', '🚫', '♦️', '✨']
+    emojis = ['🌀', '🎴', '🔮', '👑', '🎈', '🕺', '⚙️', '🍀']
 }
