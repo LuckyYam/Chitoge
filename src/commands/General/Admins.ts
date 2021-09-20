9114dd25adab87278e0a5512af7fb2cbd123b9d3
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-20 14:37:00 +0530

    Add files via upload

---
+++ src/commands/General/Admins.ts
@@ -0,0 +1,24 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'admins',
+            description: 'Tags all Admins 🎖️',
+            category: 'general',
+            usage: `${client.config.prefix}admins (Message)`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void (await M.reply(
+            `ADMINS!\n[Tags Hidden]`,
+            undefined,
+            undefined,
+            M.groupMetadata?.admins
+        ).catch((reason: any) => M.reply(`an error occupered, Reason: ${reason}`)))
+    }
+}
---
+++ src/commands/General/Profile.ts
@@ -0,0 +1,51 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'profile',
+            description: 'Displays user-profile 📜',
+            category: 'general',
+            usage: `${client.config.prefix}profile (@tag)`,
+            aliases: ['p']
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
+        let username = user === M.sender.jid ? M.sender.username : ''
+        if (!username) {
+            const contact = this.client.getContact(user)
+            username = contact.notify || contact.vname || contact.name || user.split('@')[0]
+        }
+        let pfp: string
+        try {
+            pfp = await this.client.getProfilePicture(user)
+        } catch (err) {
+            M.reply(`Profile Picture not Accessible of ${username}`)
+            pfp =
+                'https://i.pinimg.com/736x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg'
+        }
+        const data = await this.client.getUser(user)
+        await M.reply(
+            await request.buffer(
+                pfp ||
+                    'https://i.pinimg.com/736x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg'
+            ),
+            MessageType.image,
+            undefined,
+            undefined,
+            `🏮 *Username: ${username}*\n\n🎗 *About: ${
+                (await this.client.getStatus(user)).status || 'None'
+            }*\n\n⭐ *XP: ${data.Xp || 0}*\n\n👑 *Admin: ${
+                M.groupMetadata?.admins?.includes(user) || false
+            }*\n\n✖ *Ban ${data.ban || false}*`
+        )
+    }
+}
