7db8948938e90ac12c4041bdb00f94075c5b9175
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-22 19:22:48 +0530

    Update Hi.ts

--- src/commands/Misc/Hi.ts
+++ src/commands/Misc/Hi.ts
@@ -16,15 +16,15 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        // from assets/images/chitoge.png
-        const image = this.client.assets.get('chitoge')
+        // from assets/images/chitoge-source.png
+        const image = this.client.assets.get('chitoge-source')
         if (!image) return void null
         return void M.reply(
             image,
             MessageType.image,
             undefined,
             undefined,
-            `I don't have time to have a conversation with someone like you. Use something from *:help* list if you want anything.\n`
+            `I don't have time to have a conversation with someone like you. Use something from the *:help* list if you want anything.\n`
         )
     }
 }
@@ -38,7 +38,10 @@ export default class Command extends BaseCommand {
            
             
             
-       
+        
+    
+
+    
         
            
            
