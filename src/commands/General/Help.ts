7305f947abe9ac19079d8a51177d0c40c06dfd63
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-22 11:01:18 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -31,7 +31,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋 (💙ω💙) Konichiwa! *${M.sender.username}*, I'm Chitoge 💙. The usable commands are listed below.\n\n`
+            let text = `👋 (💙ω💙) Konichiwa! *${M.sender.username}*, I'm Chitoge. The usable commands are listed below.\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
