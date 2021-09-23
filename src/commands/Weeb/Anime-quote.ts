9fef73e9ac936c8b16ba38e0e5bbf765384c8a47
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-23 17:26:34 +0530

    Create Anime-quote.ts

---
+++ src/commands/Weeb/Anime-quote.ts
@@ -0,0 +1,30 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'anime-quote',
+            description: 'Will send you random anime quote',
+            aliases: ['ani-quote'],
+            category: 'fun',
+            usage: `${client.config.prefix}anime-quote`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios
+            .get(`https://animechan.vercel.app/api/random`)
+            .then((response) => {
+                // console.log(response);
+                const text = `🍀 *Anime:* ${response.data.anime}\n\n*💠 Charecter:* ${response.data.character}\n\n*🌀 Quote:* ${response.data.quote}`
+                M.reply(text)
+            })
+            .catch((err) => {
+                M.reply(`✖ An error occurred: ${err}`)
+            })
+    }
+}
