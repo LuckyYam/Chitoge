e304a9e3b88db62547ea613eadbcab648a954ff2
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-23 11:52:30 +0530

    Update Hi.ts

--- src/commands/Misc/Hi.ts
+++ src/commands/Misc/Hi.ts
@@ -1,4 +1,4 @@
-import { MessageType } from '@adiwajshing/baileys'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
@@ -16,15 +16,13 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        // from assets/images/chitoge-source.png
-        const image = this.client.assets.get('chitoge-source')
-        if (!image) return void null
-        return void M.reply(
-            image,
-            MessageType.image,
-            undefined,
-            undefined,
-            `I don't have time to have a conversation with someone like you. Use something from the *:help* list if you want anything.\n`
+        const n = [
+            './assets/videos/chitoge/chitoge-hi.mp4'
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `I don't have time to have a conversation with someone like you. Use something from *:help* list if you want anything. \n` }
         )
     }
 }
