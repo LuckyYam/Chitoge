0d75cd58e855fafd2e8266c0001e4ab1735401b5
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-23 17:19:20 +0530

    Update Rpaper.ts

--- src/commands/Weeb/Rpaper.ts
+++ src/commands/Weeb/Rpaper.ts
@@ -1,40 +1,50 @@
-import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import  axios  from 'axios'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+// import { MessageType, Mimetype } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'rpaper',
-            description: 'Will send you random anime wallpaper',
+            description: 'Will send you random Anime wallpaper.',
+            aliases: ['wallpaper'],
             category: 'weeb',
-            usage: `${client.config.prefix}rpaper`,
-            
+            usage: `${client.config.prefix}rwallpaper`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-
-
-        const rnekol = ["wallpaper"];
-        const rnekolc = rnekol[Math.floor(Math.random() * rnekol.length)];
-        const neko = await axios.get('https://nekos.life/api/v2/img/' + rnekolc)
-
-return void M.reply(await request.buffer(neko.data.url), MessageType.image, undefined, undefined, `*Here you go 🌟*`)
-
-
-
+        // fetch result of https://nekos.life/api/v2/img/wallpaper from the API using axios
+        const { data } = await axios.get('https://nekos.life/api/v2/img/wallpaper')
+        const buffer = await request.buffer(data.url).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `🌟 Here you go\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
+                console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
     }
-
-
-
-
-
-
-
-
 }
