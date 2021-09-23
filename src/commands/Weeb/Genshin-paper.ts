56f0af73a4970f4fe5c4c52927d19ea62a4f4605
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-23 14:02:48 +0530

    Create Genshin-paper.ts

---
+++ src/commands/Weeb/Genshin-paper.ts
@@ -0,0 +1,49 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'genshin-paper',
+            description: 'Will send you random Genshin Impact wallpaper.',
+            aliases: ['gpaper'],
+            category: 'weeb',
+            usage: `${client.config.prefix}genshin-paper`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // fetch result of https://zxbott.herokuapp.com/randomgangsinimpac1 from the API using axios
+        const { data } = await axios.get('https://zxbott.herokuapp.com/randomgangsinimpac1')
+        const buffer = await request.buffer(data.url).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `🌟 Well...\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
+}
