11317f7bf0443c8602b6783692a2c9b0df645aad
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-23 12:05:38 +0530

    Update Chitoge.ts

--- src/commands/Misc/Chitoge.ts
+++ src/commands/Misc/Chitoge.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/videos/chitoge/chitoge.mp4'
+            './assets/videos/chitoge/chitoge-git.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
