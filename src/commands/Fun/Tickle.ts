c556328d13836ad8565895e6b25e25227083eecf
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-24 12:08:58 +0530

    Update Tickle.ts

--- src/commands/Fun/Tickle.ts
+++ src/commands/Fun/Tickle.ts
@@ -22,12 +22,12 @@ export default class Command extends BaseCommand {
         // let username1 = user1.replace('@s.whatsapp.net', '')
         // let username2 = user2.replace('@s.whatsapp.net', '')
         const n = [
-            './assets/videos/tickle/tickle2.mp4',
-            './assets/videos/tickle/tickle1.mp4',
-            './assets/videos/tickle/tickle.mp4'
+            'https://m.imgur.com/bt2ZRjJ',
+            'https://c.tenor.com/L5-ABrIwrksAAAAC/tickle-anime.gif',
+            'https://c.tenor.com/PXL1ONAO9CEAAAAC/tickle-laugh.gif'
         ]
-        let hug = n[Math.floor(Math.random() * n.length)]
-        return void this.client.sendMessage(M.from, { url: hug }, MessageType.video, {
+        let tickle = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: tickle }, MessageType.video, {
             mimetype: Mimetype.gif,
             caption: `@${user1.split('@')[0]} tickled @${user2.split('@')[0]}`,
             contextInfo: { mentionedJid: [user1, user2] }
