3d8662b0e5ccd241f26cdc381ca0212877a2d446
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-24 10:21:52 +0530

    Update Ship.ts

--- src/commands/Fun/Ship.ts
+++ src/commands/Fun/Ship.ts
@@ -1,27 +1,20 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'ship',
             description: 'Ship!',
-            aliases: ['s'],
             category: 'fun',
-            usage: `${client.config.prefix}ship [tag]`
+            usage: `${client.config.prefix}ship [tag users]`
         })
     }
-
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        const user1 = M.sender.jid
-        const user2 = M.mentioned[0]
-        const n = [
-            './assets/videos/chitoge/ship.mp4'
-        ]
-        let chitoge = n[Math.floor(Math.random() * n.length)]
         const per = Math.floor(Math.random() * 100)
 
 if (per < 25) { 
@@ -35,14 +28,24 @@ var sentence = `${per}% Amazing! You two will be a good couple 💖 `
 } else {
 var sentence = `${per}% You two are fated to be together 💙` 
 }
-        return void M.reply(M.from, { url: chitoge }, MessageType.video, {
+        const user1 = M.sender.jid
+        const user2 = M.mentioned[0]
+        //  let username1 = user1.split('@')[0]
+        //  let username2 = user2.split('@')[0]
+        // let username1 = user1.replace('@s.whatsapp.net', '')
+        // let username2 = user2.replace('@s.whatsapp.net', '')
+        const n = [
+            './assets/videos/chitoge/ship.mp4'
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
             mimetype: Mimetype.gif,
             caption: `❣️ *Matchmaking...*
 ---------------------------------
     @${user1.split('@')[0]}  x  @${user2.split('@')[0]}
 ---------------------------------
-    ${sentence}`
-        contextInfo: { mentionedJid: [user1, user2] }
+    ${sentence}`,
+            contextInfo: { mentionedJid: [user1, user2] }
         })
     }
 }
