22f77c95863968ccf86630f99d55bde05ee82b7f
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-25 12:55:36 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -60,7 +60,7 @@ export default class MessageHandler {
                 sender.username
             )} in ${chalk.cyanBright(groupMetadata?.subject || 'DM')}`
         )
-        if (!command) return void M.reply('No such command, Baka! Have you never seen someone use the command *${client.config.prefix}help*.')
+        if (!command) return void M.reply('No such command, Baka! Have you never seen someone use the command *:help*.')
         const user = await this.client.getUser(M.sender.jid)
         if (user.ban) return void M.reply("You're Banned from using commands.")
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
