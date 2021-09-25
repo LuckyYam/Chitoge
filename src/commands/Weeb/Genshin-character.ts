f5151d1f1bdde739d427a5f8dfdddfe2584e7c20
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-25 18:46:41 +0530

    Update Genshin-character.ts

--- src/commands/Weeb/Genshin-character.ts
+++ src/commands/Weeb/Genshin-character.ts
@@ -1,40 +1,29 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'genshin-character',
+            command: 'genshin-chara',
+            description: 'Gives you the stats of the given character.',
             aliases: ['gchara'],
-            description: 'Displays the stats of the given genshin character.',
-            category: 'weeb',
-            dm: true,
-            usage: `${client.config.prefix}genshin-character [character]`
+            category: 'dev',
+            usage: `${client.config.prefix}quote`
         })
     }
-    // static count = 0
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('✖ Please provide the name of the character you wanna search.')
-        await axios
-            .get(
-                `https://api.genshin.dev/characters/albedo`
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios.get(`https://api.genshin.dev/characters/albedo`)
+        .then((response) => {
+                // console.log(response);
+                const text = `💎 *Name: ${response.data.name}*\n💠 *Vision: ${response.data.vision}*\n📛 *Weapon: ${response.data.weapon}*\n⛩ *Nation: ${response.data.nation}*\n📛 *Affiliation: ${response.data.affiliation}*\n❄ *Constellation: ${response.data.constellation}*\n🎗 *Rarity: ${response.data.rarity}*\n🎁 *Birthday: ${response.data.birthday}*\n💚 *Description: ${response.data.description}* `
+                M.reply(text);
+            }).catch(err => {
+                M.reply(`✖ An error occurred: ${err}`)
+            }
             )
-            .then((res) => {
-       // if (res.status !== 200) return void M.reply(`✖ Error: ${res.status}`)
-                  let result = ``
-                  let index = 1
-                for (const item of res.data?.items) {
-                    result += `🌟 *Name: Albedo*\n💠 *Vision: ${res.data.vision}*\n🎋 *Nation: ${res.data.nation}\n📛 *Affiliation: ${res.data.affiliation}*\n♦️ *Rarity: ${res.data.rarity}* 🌟\n❄ *Constellation: ${res.data.constellation}\n🎁 *Birthday: ${res.data.birthday}\n📒 *Description: ${res.data.description}  `
-                    index++
-                }
-                // return void M.reply(`🔍 Command Used : Result for *${term} character*\n\n\n ${result}`)
-                return void M.reply(`${result}`)
-            })
-            .catch((err) => {
-                M.reply(`🔍 Error: ${err}`)
-            })
-    }
+    };
 }
