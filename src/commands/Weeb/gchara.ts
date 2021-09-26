3682fafe399fa80c93d3802e85ff6fc5a46c715d
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-09-26 07:32:58 +0530

    Update gchara.ts

--- src/commands/Weeb/gchara.ts
+++ src/commands/Weeb/gchara.ts
@@ -1,3 +1,4 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
