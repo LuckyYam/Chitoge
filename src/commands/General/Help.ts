ffd2199ce52b19520dee48fef22e4b6100ed2d01
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-11-11 17:23:59 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -36,7 +36,7 @@ export default class Command extends BaseCommand {
 					categories[info.config.category].push(info);
 				}
 			}
-			let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*, I'm Chitoge.\n\nMy prefix is - ":"\n\nThe usable commands are listed below.\n\n`;
+			let text = `👋🏻 (💙ω💙) Konichiwa! *${M.sender.username}*, I'm Chitoge.\n\nMy prefix is - "${this.client.config.prefix}"\n\nThe usable commands are listed below.\n\n`;
 			const keys = Object.keys(categories);
 			for (const key of keys)
 				text += `${
