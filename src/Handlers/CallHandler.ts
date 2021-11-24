eaf5a8e97bcdbfde8caeaa7c2435f13d923a247c
ShinNouzen <johncena795001@gmail.com>
2021-11-24 13:21:56 +0530

    Will block the user who called the bot.

--- src/Handlers/CallHandler.ts
+++ src/Handlers/CallHandler.ts
@@ -33,7 +33,7 @@ export default class CallHandler {
         await this.client.sendWA(`${tag},${JSON.stringify(json)}`)
         await this.client.sendMessage(
 					caller,
-					`You'll be blocked for calling the bot.`,
+					`You'll be blocked for calling the bot, Baka!`,
 					MessageType.text
 				);
 				await this.client.blockUser(caller);
