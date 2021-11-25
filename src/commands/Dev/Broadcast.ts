35ece1a1eaa8032f9431ba01cdc5736101630a4b
ShinNouzen <johncena795001@gmail.com>
2021-11-25 13:17:03 +0530

    Changed the format of `Broadcast` Message from `gif` to `image`

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { MessageType } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
@@ -28,16 +28,15 @@ export default class Command extends BaseCommand {
 		if (!joined)
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
-		const gifs = [
-			"https://media.tenor.com/videos/b5bb295fb219e5cd12cb74d29eaa079c/mp4",
-			"https://media.tenor.com/videos/cb603deb19eefdb7feee3aa7aa1aa7b6/mp4",
-			"https://media.tenor.com/videos/31edd50dd5096731be4690a67142a1aa/mp4",
-			"https://media.tenor.com/videos/8fb551bffa77b6f6d875eb0a0a5e8fa9/mp4",
-			"https://media.tenor.com/videos/e09c85e729650f03ca9099663718e38c/mp4",
-			"https://media.tenor.com/videos/9eda38308ee0b60c51962dde63d203c7/mp4",
-			"https://media.tenor.com/videos/f4c3cd17a4348142d254a1f5f206a0d7/mp4",
+		const images = [
+			"https://wallpapercave.com/wp/wp3144753.jpg",
+			"https://wallpapercave.com/wp/wp4782018.jpg",
+			"https://wallpaperaccess.com/full/1326836.jpg",
+			"https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg",
+			"https://data.whicdn.com/images/304776416/original.jpg",
+			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
 		];
-		const selected = gifs[Math.floor(Math.random() * gifs.length)];
+		const selected = images[Math.floor(Math.random() * images.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const chats: any = this.client.chats
 			.all()
@@ -47,8 +46,7 @@ export default class Command extends BaseCommand {
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
 			const text = `*🌟「 CHITOGE BROADCAST 」🌟*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
-			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
-				mimetype: Mimetype.gif,
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
 				caption: `${text}`,
 				contextInfo: {
 					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
