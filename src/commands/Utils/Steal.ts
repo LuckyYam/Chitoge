4f8030e57968a05308965fa3347ccb2f8d574390
ShinNouzen <johncena795001@gmail.com>
2021-11-27 08:26:23 +0530

    Fixed

--- src/commands/Utils/Steal.ts
+++ src/commands/Utils/Steal.ts
@@ -8,6 +8,7 @@
 
 /*eslint-disable @typescript-eslint/no-explicit-any */
 /*eslint-disable @typescript-eslint/no-unused-vars */
+/*eslint-disable @typescript-eslint/explicit-module-boundary-types*/
 
 import { MessageType, Mimetype } from "@adiwajshing/baileys";
 import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
