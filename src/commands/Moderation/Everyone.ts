5aadd2486e1db887b7e3d1e53a2eaa0479eb362b
ShinNouzen <johncena795001@gmail.com>
2021-11-27 14:13:05 +0530

    `Ping` with sticker.

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -1,28 +1,74 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'everyone',
-            description: 'Tags all users in group chat',
-            aliases: ['all', 'tagall', 'ping'],
-            category: 'moderation',
-            usage: `${client.config.prefix}everyone`,
-            adminOnly: true,
-            baseXp: 20
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "everyone",
+			description: "Tags all users in group chat",
+			aliases: ["all", "tagall", "ping"],
+			category: "moderation",
+			usage: `${client.config.prefix}everyone`,
+			adminOnly: true,
+			baseXp: 20,
+		});
+	}
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(
-            `${M.groupMetadata?.subject || '*EVERYONE*'}\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
-            undefined,
-            undefined,
-            M.groupMetadata?.participants.map((user) => user.jid)
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ).catch((reason: any) => M.reply(`✖️ An error occurred, Reason: ${reason}`)))
-    }
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		const stickers = [
+			"https://wallpapercave.com/wp/wp3144753.jpg",
+			"https://wallpapercave.com/wp/wp4782018.jpg",
+			"https://wallpaperaccess.com/full/1326836.jpg",
+			"https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg",
+			"https://data.whicdn.com/images/304776416/original.jpg",
+			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
+		];
+		const option = ["--s"];
+		const random = stickers[Math.floor(Math.random() * stickers.length)];
+		if (!joined)
+			return void (await M.reply(
+				`${
+					M.groupMetadata?.subject || "*EVERYONE*"
+				}\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
+				undefined,
+				undefined,
+				M.groupMetadata?.participants.map((user) => user.jid)
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			).catch((reason: any) =>
+				M.reply(`✖️ An error occurred, Reason: ${reason}`)
+			));
+		const selected = joined.trim();
+		if (!option.includes(selected))
+			return void (await M.reply(
+				`${
+					M.groupMetadata?.subject || "*EVERYONE*"
+				}\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
+				undefined,
+				undefined,
+				M.groupMetadata?.participants.map((user) => user.jid)
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			).catch((reason: any) =>
+				M.reply(`✖️ An error occurred, Reason: ${reason}`)
+			));
+		const sticker: any = await new Sticker(random, {
+			pack: "READ QUOTED MESSAGE",
+			author: "🌟 Chitoge 🌟",
+			quality: 90,
+			type: "full",
+			categories: ["🎊"],
+		});
+		return void (await M.reply(
+			await sticker.build(),
+			MessageType.sticker,
+			Mimetype.webp,
+			M.groupMetadata?.participants.map((user) => user.jid)
+		));
+	};
 }
