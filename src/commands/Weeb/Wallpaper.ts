1e978a94598838da600e4e58857022b3548d7bf2
ShinNouzen <johncena795001@gmail.com>
2021-11-29 16:24:22 +0530

    `Wallpaper` changed

--- src/commands/Weeb/Wallpaper.ts
+++ src/commands/Weeb/Wallpaper.ts
@@ -5,8 +5,8 @@ import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import request from "../../lib/request";
 import { MessageType } from "@adiwajshing/baileys";
-
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
@@ -24,43 +24,53 @@ export default class Command extends BaseCommand {
 		{ joined }: IParsedArgs
 	): Promise<void> => {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const chitoge: any = joined.trim().split("|");
-		const term: string = chitoge[0];
-		const amount: number = chitoge[1];
-		if (term === "")
-			return void M.reply(
-				`Give me the wallpaper term and page to search, Baka!`
-			);
-		if (!amount)
-			return void M.reply(
-				`Give me the number of wallpapers to send, Baka!\n\nExample: *${this.client.config.prefix}wallpaper chitoge|5*`
-			);
-		if (amount > 20)
-			return void M.reply(`Do you want me to spam in this group?`);
+		if (!joined)
+			return void (await M.reply(`Give me a wallpaper term to search, Baka!`));
+		const chitoge = joined.trim();
 		const wall = new AnimeWallpaper();
-		const pages = [1, 2, 3, 4, 5];
+		const pages = [1, 2, 3, 4];
 		const random = pages[Math.floor(Math.random() * pages.length)];
 		const wallpaper = await wall
-			.getAnimeWall4({ title: term, type: "sfw", page: random })
+			.getAnimeWall4({ title: chitoge, type: "sfw", page: random })
 			.catch(() => null);
 		if (!wallpaper)
 			return void (await M.reply(
 				`Couldn't find any matching term of wallpaper.`
 			));
-		if (amount > wallpaper.length) return void (await M.reply(`*Try again.*`));
-		for (let i = 0; i < amount; i++) {
-			const res = `*🌟 Here you go.*`;
-			this.client.sendMessage(
-				M.from,
-				{ url: wallpaper[i].image },
-				MessageType.image,
-				{
-					quoted: M.WAMessage,
-					caption: `${res}`,
-				}
-			);
+		const i = Math.floor(Math.random() * wallpaper.length);
+		const buffer = await request.buffer(wallpaper[i].image).catch((e) => {
+			return void M.reply(e.message);
+		});
+		while (true) {
+			try {
+				M.reply(
+					buffer || "✖ An error occurred. Please try again later.",
+					MessageType.image,
+					undefined,
+					undefined,
+					`*🌟 Here you go.*`,
+					undefined
+				).catch((e) => {
+					console.log(
+						`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
+					);
+					// console.log('Failed')
+					M.reply(`✖ An error occurred. Please try again later.`);
+				});
+				break;
+			} catch (e) {
+				// console.log('Failed2')
+				M.reply(`✖ An error occurred. Please try again later.`);
+				console.log(
+					`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
+				);
+			}
 		}
+		return void null;
 	};
 }
+
+		
+
 	
 		
