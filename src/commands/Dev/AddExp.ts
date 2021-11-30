033bdeb4d51b6e8902acc1f85e0e6a79db1d69bb
ShinNouzen <johncena795001@gmail.com>
2021-11-30 19:48:41 +0530

    ...

--- src/commands/Dev/AddExp.ts
+++ src/commands/Dev/AddExp.ts
@@ -4,7 +4,6 @@ import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-import { evaluate } from "mathjs";
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
@@ -27,8 +26,7 @@ export default class Command extends BaseCommand {
 		if (M.quoted?.sender) M.mentioned.push(M.quoted.sender);
 		if (M.quoted?.sender) M.mentioned.push(M.quoted.sender);
 		const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid;
-		(await this.client.getUser(user)).Xp = await evaluate(
-			(await (await this.client.getUser(user)).Xp) + amount
-		);
+		(await this.client.getUser(user)).Xp = amount;
+		await M.reply(`Successfully added ${amount} of exp.`);
 	};
 }
