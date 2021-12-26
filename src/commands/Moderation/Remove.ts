184ebda97fd84c2b5526a3ba791b6aac2c5bc80a
Shinei Ichijo <85594230+ShineiIchijo@users.noreply.github.com>
2021-12-26 17:43:27 +0530

    Update Remove.ts

--- src/commands/Moderation/Remove.ts
+++ src/commands/Moderation/Remove.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             adminOnly: true,
-            aliases: ['boom', 'kick'],
+            aliases: ['boom'],
             command: 'remove',
             description: 'removes the mentioned users',
             category: 'moderation',
