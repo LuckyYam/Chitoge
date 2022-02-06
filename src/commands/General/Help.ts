6e5ed53cb3102572debeea7b94c320f7e7581928
Restyled.io <commits@restyled.io>
2022-02-06 04:09:25 +0000

    Restyled by whitespace

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
 			let text = `👋🏻 (💙ω💙) Konichiwa! *@${
 				user.split("@")[0]
 			}*,\n━❰I'm AYUSH a whatsapp bot for your assistance*tell me how can i help you?❱━\n it's my official group:[https://chat.whatsapp.com/E5CwW1dAXjRKE3XuLXxF8J]\n\nMy prefix is - "${
-				this.client.config.prefix 
+				this.client.config.prefix
 			}"\n\nThe usable commands are listed below.\n\n`;
 			const keys = Object.keys(categories);
 			for (const key of keys)
