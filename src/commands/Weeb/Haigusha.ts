c7fc8f3f30479ccb30f7745fe466db344f91a4cb
ShineiIchijo <johncena795001@gmail.com>
2022-03-10 14:51:03 +0530

    updating some commands

--- src/commands/Weeb/Haigusha.ts
+++ src/commands/Weeb/Haigusha.ts
@@ -1,4 +1,4 @@
-import { Character } from "@shineiichijo/marika";
+import MWL, { IWaifuResponse } from "../../lib/MWL";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
@@ -18,33 +18,31 @@ export default class Command extends BaseCommand {
   }
 
   run = async (M: ISimplifiedMessage): Promise<void> => {
-    const client = new Character();
-    let haigusha;
-    try {
-      haigusha = await client.getRandomCharacter();
-    } catch (error) {
-      return void M.reply("✖ An error occurred. Please try again later");
-    }
-    let source;
-    try {
-      source = await client.getCharacterAnime(haigusha.mal_id);
-    } catch (error) {
-      return void M.reply("✖ An error occurred. Please try again later");
-    }
+    const random = await MWL()
+    if ((random as { error: string }).error)
+			return void (await M.reply(
+				"✖ An error occurred. Please try again later."
+			));
+    const haigusha = random as IWaifuResponse
     let text = "";
-    text += `💙 *Name: ${haigusha.name}*\n`;
-    if (haigusha.nicknames.length > 0)
-      text += `💚 *Nicknames: ${haigusha.nicknames.join(", ")}*\n`;
-    text += `🖤 *Favorites: ${haigusha.favorites}*\n`;
-    text += `💛 *Source: ${source.data[0].anime.title}*\n\n`;
-    text += `🌐 *URL: ${haigusha.url}*\n\n`;
-    if (haigusha.about !== null) text += `❤ *Description:* ${haigusha.about}`;
-    return void M.reply(
-      await this.client.getBuffer(haigusha.images.jpg.image_url),
-      MessageType.image,
-      undefined,
-      undefined,
-      text
-    );
+		text += `💙 *Name: ${haigusha.name}*\n`;
+		if (haigusha.original_name !== "" && haigusha.original_name !== null)
+			text += `💚 *Original Name: ${haigusha.original_name}*\n`;
+		if (haigusha.weight !== null) text += `⚖ *Weight: ${haigusha.weight}*\n`;
+		if (haigusha.height !== null) text += `📍 *Height: ${haigusha.height}*\n`;
+		if (haigusha.bust !== null) text += `💠 *Bust: ${haigusha.bust}*\n`;
+		if (haigusha.hip !== null) text += `🎗 *Hip: ${haigusha.hip}*\n`;
+		if (haigusha.waist !== null) text += `🎀 *Waist: ${haigusha.waist}*\n`;
+		if (haigusha.blood_type !== null)
+			text += `🩸 *Blood Type: ${haigusha.blood_type}*\n`;
+		if (haigusha.origin !== null && haigusha.origin !== "") text += `🎐 *Origin: ${haigusha.origin}*\n`;
+		if (haigusha.age !== null&& haigusha.age !== 0) text += `🎂 *Age: ${haigusha.age}*\n`;
+		if (haigusha.likes !== null) text += `🖤 *Likes: ${haigusha.likes}*\n`;
+		text += `🏅 *Like Rank: ${haigusha.like_rank}*\n`;
+		text += `📈 *Popularity Rank: ${haigusha.popularity_rank}*\n\n`;
+		text += `💛 *Source: ${haigusha.series.name}*\n\n`;
+		text += `🌐 *URL: ${haigusha.url}*\n\n`;
+		text += `❤ *Description:* ${haigusha.description}\n`;
+    return void M.reply(await this.client.getBuffer(haigusha.display_picture), MessageType.image, undefined, undefined, text)
   };
 }
---
+++ src/lib/MWL.ts
@@ -0,0 +1,63 @@
+import axios from "axios"
+
+export interface IWaifuResponse {
+    id: number;
+      slug: string;
+      name: string;
+      original_name: string;
+      romaji_name: string;
+      display_picture: string;
+      description: string;
+      weight: string;
+      height: string;
+      bust: string;
+      hip: string;
+      waist: string;
+      blood_type: string;
+      origin: string;
+      age: number;
+      birthday_month: string;
+      birthday_day: number;
+      birthday_year: string;
+      likes: number;
+      trash: number;
+      popularity_rank: number;
+      like_rank: number;
+      trash_rank: number;
+      husbando: boolean;
+      nsfw: boolean;
+      creator: {
+        id: number;
+        name: string;
+      },
+      tags: {
+           id: number;
+           name: string ;
+        }[];
+      url: string;
+      appearances: ISeries[]
+      series: ISeries
+}
+
+export default async (url = "https://reina-api.herokuapp.com/api/mwl/random"): Promise<IWaifuResponse | { error: string }> => {
+ try {
+   const { data } = await axios.get(url)
+   return data
+ } catch (error) {
+   return { error: "An error occurred. Try again later"}
+ }
+}
+
+interface ISeries {
+    name: string;
+          original_name: string;
+          romaji_name: string;
+          description: string;
+          slug: string;
+          airing_start: string;
+          airing_end: string;
+          episode_count: string;
+          release: string;
+          display_picture: string;
+          studio: string;
+}
